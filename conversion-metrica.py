0253ce5b6b456ccff20c348904c759f81dc5212b
lizfore <andresrivera@192.168.0.11>
2026-06-04 07:09:49 -0500

    Conversion de cm a pulgadas

---
+++ conversion-metrica.py
@@ -0,0 +1,5 @@
+medida_cms = float(input("Por favor ingresa las medidas de la pieza del mueble (cms): "))
+
+medida_pulgadas = medida_cms / 2.54
+
+print(f"Las medidas en pulgadas de la pieza ingresada son: {medida_pulgadas:.2f}")
